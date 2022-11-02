79c7c1ba7714106e0a20ce9cf766f7be9e15c7ed
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-11-03 01:27:56 +0900

    rename: move modules to src/reversi_game

---
+++ src/reversi_game/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import random
+from .color import Color
+from .reversi import Reversi
+
+
+reversi = Reversi(8, 8)
+player_turn = random.choice([Color.DARK, Color.LIGHT])
+while reversi.turn != Color.NONE:
+    print(reversi.board)
+    row, column = map(int, input(f'color={reversi.turn} R C > ').split())
+    reversi.place_disk(row, column, reversi.turn)
+    reversi.turn = reversi.forward_turn()
+    # break
